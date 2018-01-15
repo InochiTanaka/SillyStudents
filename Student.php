7ce64f40bc7fda05304b43aac2ec998f4777a3ee
yunomiMac\yunomi <yunominosencha@gmail.com>
2018-01-14 16:31:39 -0800

    add comments on the code

--- Student.php
+++ Student.php
@@ -1,17 +1,12 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Student
  *
- * @author yunomi
+ * @author Inochi Tanaka
  */
 class Student {
+    
+    //add functions to show infomation of student
    function __construct() {
        $this -> surname = '';
        $this->first_namew = '';
